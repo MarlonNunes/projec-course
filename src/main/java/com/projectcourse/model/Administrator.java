0fb2e6cf184c1a046bb331fbfa119524611b6d1a
Marlon Nunes <marlonnunes869@gmail.com>
2021-08-14 01:56:35 -0300

    Added administrator class

---
+++ src/main/java/com/projectcourse/model/Administrator.java
@@ -0,0 +1,14 @@
+package com.projectcourse.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Administrator extends User{
+
+    @Id
+    private Integer idAdmin;
+}
