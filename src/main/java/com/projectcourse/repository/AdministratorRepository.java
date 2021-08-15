f54a0136bea88134e118e10b8dadeedab35700b3
Marlon Nunes <marlonnunes869@gmail.com>
2021-08-15 02:43:33 -0300

    Added AdministratorService

--- src/main/java/com/projectcourse/repository/AdministratorRepository.java
+++ src/main/java/com/projectcourse/repository/AdministratorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
+    Administrator findByUsername (String username);
 }
---
+++ src/main/java/com/projectcourse/service/AdministratorService.java
@@ -0,0 +1,44 @@
+package com.projectcourse.service;
+
+import com.projectcourse.model.Administrator;
+import com.projectcourse.repository.AdministratorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdministratorService implements UserDetailsService {
+
+    private final AdministratorRepository administratorRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return Optional.ofNullable(administratorRepository.findByUsername(username))
+                .orElseThrow(()-> new UsernameNotFoundException("Admin Not Found"));
+    }
+
+    public Administrator findById(Integer id){
+        return administratorRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Admin Not Found"));
+    }
+
+    public Administrator save(Administrator administrator){
+
+        return administratorRepository.save(administrator);
+    }
+
+    public void delete (Integer id){
+         administratorRepository.deleteById(id);
+    }
+
+    public Administrator replace(Administrator administrator){
+        return administratorRepository.save(administrator);
+    }
+}
