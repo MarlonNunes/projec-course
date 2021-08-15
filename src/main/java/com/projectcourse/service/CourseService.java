d624c26eb814ca6f253498d8a05305266029f6fb
Marlon Nunes <marlonnunes869@gmail.com>
2021-08-15 03:04:49 -0300

    Added CourseService

---
+++ src/main/java/com/projectcourse/service/CourseService.java
@@ -0,0 +1,43 @@
+package com.projectcourse.service;
+
+import com.projectcourse.model.Course;
+import com.projectcourse.model.Student;
+import com.projectcourse.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public List<Course> findAll(){
+
+        return courseRepository.findAll();
+    }
+
+    public Course findById(Integer id){
+        return courseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found"));
+    }
+
+    public Course save(Course course){
+
+        return courseRepository.save(course);
+    }
+
+    public void delete(Integer id){
+        courseRepository.deleteById(id);
+    }
+
+    public Course Replace (Course course){
+
+        return courseRepository.save(course);
+    }
+}
+
